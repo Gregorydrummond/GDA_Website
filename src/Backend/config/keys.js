8a85ac741f70de29918a2dc76ec8e48af3794c8e
Greg <Gregorydrummond45@gmail.com>
2021-03-06 12:07:57 -0500

    Setup React and Jest Testing

---
+++ src/Backend/config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://Gregory:hNNVQJQrl7KcsF6T@cluster0.rl5bz.mongodb.net/Cluster0?retryWrites=true&w=majority'
+}
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+//import Server from './Backend/server';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
